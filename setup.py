4c1bb7f405265fc32841096d6a13930384cdc19e
Atlas-dev-project <software@kampusmedia.ca>
2024-09-06 13:20:25 -0400

    change PyPip version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='mypythonlib_project',
     packages=find_packages(),
-    version='0.1.0',
+    version='0.1.1',
     description='My Python library',
     author='Nasim',
 )
